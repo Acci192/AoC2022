8fc5b6da6ea23eac5160b9ae1884404e2a0ef8b7
Andreas Karlsson <k.andreas@live.se>
2022-12-03 09:50:32 +0100

    Day 4 Preparation

---
+++ SolutionTests/Day4Tests.cs
@@ -0,0 +1,25 @@
+using aoc2022.solutions;
+using FluentAssertions;
+
+namespace SolutionTests;
+
+public class Day4Tests
+{
+    public ASolution Solution { get; set; }
+    public Day4Tests()
+    {
+        Solution = new Day4(true);
+    }
+
+    [Fact]
+    public void A()
+    {
+        Solution.A().Should().Be("");
+    }
+
+    [Fact]
+    public void B()
+    {
+        Solution.B().Should().Be("");
+    }
+}
---
+++ aoc2022/solutions/Day4.cs
@@ -0,0 +1,16 @@
+﻿namespace aoc2022.solutions;
+
+public class Day4 : ASolution
+{
+    public Day4(bool testInput) : base(testInput) { }
+
+    public override string A()
+    {
+        return string.Empty;
+    }
+
+    public override string B()
+    {
+        return string.Empty;
+    }
+}
