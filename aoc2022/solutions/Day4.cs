31d55853d572f7e3f93eb4166bf417b2960d15b7
Andreas Karlsson <k.andreas@live.se>
2022-12-04 07:06:05 +0100

    Improved Day 4 solution

--- aoc2022/solutions/Day4.cs
+++ aoc2022/solutions/Day4.cs
@@ -6,44 +6,20 @@ public class Day4 : ASolution
 
     public override string A()
     {
-        var pairs = GetPairs(Input);
-
-        var sum = 0;
-        foreach(var pair in pairs)
-        {
-            if(pair.first.Start <= pair.second.Start 
-                && pair.first.End >= pair.second.End)
-            {
-                sum++;
-            }
-            else if(pair.second.Start <= pair.first.Start
-                && pair.second.End >= pair.first.End)
-            {
-                sum++;
-            }
-        }
-
-        return sum.ToString();
+        return GetPairs(Input)
+            .Count(pair => (pair.first.Start <= pair.second.Start && pair.first.End >= pair.second.End)
+                || (pair.second.Start <= pair.first.Start && pair.second.End >= pair.first.End))
+            .ToString();
     }
 
     public override string B()
     {
-        var pairs = GetPairs(Input);
-
-        var sum = 0;
-        foreach (var (first, second) in pairs)
-        {
-            var test = Enumerable.Range(first.Start, first.End - first.Start + 1).ToList();
-            var test1 = Enumerable.Range(second.Start, second.End - second.Start + 1).ToList();
-
-            if (test.Any(x => test1.Contains(x)))
-                sum++;
-        }
-
-        return sum.ToString();
+        return GetPairs(Input)
+            .Count(pair => pair.first.Start <= pair.second.End && pair.second.Start <= pair.first.End)
+            .ToString();
     }
 
-    public IEnumerable<(Assignment first, Assignment second)> GetPairs(IEnumerable<string> input)
+    private static IEnumerable<(Assignment first, Assignment second)> GetPairs(IEnumerable<string> input)
     {
         foreach(var row in input)
         {
@@ -51,17 +27,17 @@ public class Day4 : ASolution
             yield return (new Assignment(split[0]), new Assignment(split[1]));
         }
     }
-}
 
-public class Assignment
-{
-    public int Start { get; set; }
-    public int End { get; set; }
-    
-    public Assignment(string input)
+    private class Assignment
     {
-        var split = input.Split('-');
-        Start = int.Parse(split[0]);
-        End = int.Parse(split[1]);
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        public Assignment(string input)
+        {
+            var split = input.Split('-');
+            Start = int.Parse(split[0]);
+            End = int.Parse(split[1]);
+        }
     }
 }
